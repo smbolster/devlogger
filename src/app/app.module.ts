e9c61f171e7459c1ff99af79245aa4f86674c37a
smbolster <smbolster@gmail.com>
2018-05-23 15:05:30 -0400

    added components
    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavabarComponent } from './components/navabar/navabar.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogsComponent } from './components/logs/logs.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavabarComponent,
+    LogFormComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule
