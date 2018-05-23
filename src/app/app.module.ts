9de5dc2de1addc94caa2343c5400f5624054e735
smbolster <smbolster@gmail.com>
2018-05-23 15:18:03 -0400

    added design stuff
    bootstrap, jquery, popper js and font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NavabarComponent } from './components/navabar/navabar.component';
 import { LogFormComponent } from './components/log-form/log-form.component';
 import { LogsComponent } from './components/logs/logs.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -12,7 +11,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavabarComponent,
     LogFormComponent,
     LogsComponent,
     NavbarComponent
